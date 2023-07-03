3c6482d702d2d0f262409fa1397e5ca029cd107c
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-07-03 20:48:06 +0530

    Test : Add test one spare to spare followed by a subsequent roll knocks.

--- src/test/java/com/kata/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/kata/bowlinggame/BowlingGameTest.java
@@ -42,6 +42,14 @@ public class BowlingGameTest {
 		rollMany(20, 0);
 		assertEquals(0, bowlingGame.score());
 	}
+	
+	@Test
+	public void testOneSpare() {
+		spare();
+		bowlingGame.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, bowlingGame.score());
+	}
 
 	private void rollMany(int n, int pins) {
 		for (int i = 0; i < n; i++) {
