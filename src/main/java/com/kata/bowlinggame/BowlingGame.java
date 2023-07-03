08251a3a911a3a883b9c5bcb79f3d21f3d378546
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-07-03 19:59:52 +0530

    Test: Add testcase to return the roll spare in tenpin bowling game.

--- src/main/java/com/kata/bowlinggame/BowlingGame.java
+++ src/main/java/com/kata/bowlinggame/BowlingGame.java
@@ -2,10 +2,13 @@ package com.kata.bowlinggame;
 
 public class BowlingGame {
 
-
 	public int roll(int pins) {
 		return pins;
 
+	}
+
+	public int score() {		
+		return 10;
 	}
 
 }
--- src/test/java/com/kata/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/kata/bowlinggame/BowlingGameTest.java
@@ -12,5 +12,15 @@ public class BowlingGameTest {
 	    int rollResult = bowlingGame.roll(pins); // Store the return value in a variable
 	    assertEquals(pins, rollResult);
 	}
+	
+	@Test
+	public void testRollSpare() {
+		BowlingGame bowlingGame = new BowlingGame();
+		bowlingGame.roll(5);
+		bowlingGame.roll(5);
+	    int expectedScore = 10; // The expected score for rolling a spare
+	    int actualScore = bowlingGame.score();
+	    assertEquals(expectedScore, actualScore);
+	}
 
 }
