4a8c09e8195b4e8d139ec23f8e165c757a4cea6e
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-07-03 21:03:23 +0530

    Test: Add test to return a perfect game in tenpin bowling. strike in every frame, including bonus rolls in the 10th frame.

--- src/test/java/com/kata/bowlinggame/BowlingGameTest.java
+++ src/test/java/com/kata/bowlinggame/BowlingGameTest.java
@@ -6,43 +6,44 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
-	
+
 	private BowlingGame bowlingGame;
+
 	@Before
 	public void setUp() {
-		  bowlingGame = new BowlingGame();
+		bowlingGame = new BowlingGame();
 	}
-	
+
 	@Test
-	public void testRollStrike() {	
-	    int expectedPins = strike();
-	    int rollResult = bowlingGame.roll(expectedPins); // Store the return value in a variable
-	    assertEquals(expectedPins, rollResult);
+	public void testRollStrike() {
+		int expectedPins = strike();
+		int rollResult = bowlingGame.roll(expectedPins); // Store the return value in a variable
+		assertEquals(expectedPins, rollResult);
 	}
-	
+
 	private int spare() {
-	    int firstRoll = bowlingGame.roll(5);
-	    int secondRoll = bowlingGame.roll(5);
-	    return firstRoll + secondRoll;
+		int firstRoll = bowlingGame.roll(5);
+		int secondRoll = bowlingGame.roll(5);
+		return firstRoll + secondRoll;
 	}
-	
+
 	private int strike() {
 		return bowlingGame.roll(10);
 	}
-	
+
 	@Test
-	public void testRollSpare() {	
-	    int expectedScore = spare(); // The expected score for rolling a spare
-	    int actualScore = bowlingGame.score();
-	    assertEquals(expectedScore, actualScore);
+	public void testRollSpare() {
+		int expectedScore = spare(); // The expected score for rolling a spare
+		int actualScore = bowlingGame.score();
+		assertEquals(expectedScore, actualScore);
 	}
-	
+
 	@Test
 	public void testGutterGame() {
 		rollMany(20, 0);
 		assertEquals(0, bowlingGame.score());
 	}
-	
+
 	@Test
 	public void testOneSpare() {
 		spare();
@@ -50,7 +51,7 @@ public class BowlingGameTest {
 		rollMany(17, 0);
 		assertEquals(16, bowlingGame.score());
 	}
-	
+
 	@Test
 	public void testOneStrike() {
 		strike();
@@ -60,11 +61,17 @@ public class BowlingGameTest {
 		assertEquals(24, bowlingGame.score());
 	}
 
+	@Test
+	public void testPerfectGame() {
+		rollMany(12, 10);
+		assertEquals(300, bowlingGame.score());
+	}
+
 	private void rollMany(int n, int pins) {
 		for (int i = 0; i < n; i++) {
 			bowlingGame.roll(pins);
 		}
-		
+
 	}
 
 }
